3aa6495920e31eb76a2dc7bce2f637db0c9a3fea
Jens Schulze <jens.schulze@commercetools.de>
2015-12-09 18:54:59 +0100

    add more specs

--- spec/ExampleApi/ClientSpec.php
+++ spec/ExampleApi/ClientSpec.php
@@ -36,9 +36,7 @@ class ClientSpec extends ObjectBehavior
 
         $this->beConstructedWith($options);
 
-        $client = $this->getHttpClient();
-        $request = $this->resources->hello->get();
-        $response = $client->send($request);
+        $response = $this->resources->hello->get();
         $this->validateBaseUriResponse($response);
     }
 
@@ -62,16 +60,13 @@ class ClientSpec extends ObjectBehavior
 
         $this->beConstructedWith($options);
 
-        $client = $this->getHttpClient();
-        $request = $this->resources->hello->get();
-        $response = $client->send($request);
+        $response = $this->resources->hello->get();
         $this->validateBaseUriResponse($response);
     }
 
     function it_should_append_query_string()
     {
-        $request = $this->resources->bounce->url->get(['key' => 'string']);
-        $response = $this->getHttpClient()->send($request);
+        $response = $this->resources->bounce->url->get(['key' => 'string']);
 
         $response->getStatusCode()->shouldBe(200);
         $response->getBody()->__toString()->shouldBeEqualTo('/bounce/url?key=string');
@@ -79,8 +74,7 @@ class ClientSpec extends ObjectBehavior
 
     function it_should_append_query_string_array()
     {
-        $request = $this->resources->bounce->url->get(null, ['query' => ['key' => [1,2,3]]]);
-        $response = $this->getHttpClient()->send($request);
+        $response = $this->resources->bounce->url->get(null, ['query' => ['key' => [1,2,3]]]);
 
         $response->getStatusCode()->shouldBe(200);
         $response->getBody()->__toString()->shouldBeEqualTo('/bounce/url?key=1&key=2&key=3');
@@ -88,11 +82,10 @@ class ClientSpec extends ObjectBehavior
 
     function it_should_merge_body_and_query()
     {
-        $request = $this->resources->bounce->url->get(
+        $response = $this->resources->bounce->url->get(
             ['test' => 'test', 'abc' => 123, 'key' => [1,2,3]],
             ['query' => ['xyz' => '123']]
         );
-        $response = $this->getHttpClient()->send($request);
 
         $response->getStatusCode()->shouldBe(200);
         $response->getBody()->__toString()->shouldBeEqualTo('/bounce/url?abc=123&key=1&key=2&key=3&test=test&xyz=123');
@@ -100,11 +93,10 @@ class ClientSpec extends ObjectBehavior
 
     function it_should_be_a_canonical_query()
     {
-        $request = $this->resources->bounce->url->get(
+        $response = $this->resources->bounce->url->get(
             null,
             ['query' => ['test' => 'test', 'xyz' => '123', 'abc' => 123, 'key' => [1,2,3]]]
         );
-        $response = $this->getHttpClient()->send($request);
 
         $response->getStatusCode()->shouldBe(200);
         $response->getBody()->__toString()->shouldBeEqualTo('/bounce/url?abc=123&key=1&key=2&key=3&test=test&xyz=123');
@@ -112,8 +104,7 @@ class ClientSpec extends ObjectBehavior
 
     function it_should_pass_custom_headers_with_the_request()
     {
-        $request = $this->resources->bounce->headers->get(null, ['headers' => ['X-Custom-Header' =>  'Custom Header']]);
-        $response = $this->getHttpClient()->send($request);
+        $response = $this->resources->bounce->headers->get(null, ['headers' => ['X-Custom-Header' =>  'Custom Header']]);
 
         $response->getStatusCode()->shouldBe(200);
         $response->getBody()->__toString()->shouldJsonBeEqualTo('Custom Header', 'x-custom-header');
@@ -121,18 +112,60 @@ class ClientSpec extends ObjectBehavior
 
     function it_should_use_default_headers_from_definition()
     {
-        $request = $this->resources->defaults->headers->get();
-        $response = $this->getHttpClient()->send($request);
+        $response = $this->resources->defaults->headers->get();
         $response->getStatusCode()->shouldBe(200);
         $response->getBody()->__toString()->shouldJsonBeEqualTo('Hello World!', 'x-default-header');
     }
 
     function it_should_override_default_headers()
     {
-        $request = $this->resources->defaults->headers->get(null, ['headers' => ['x-default-header' => 'Overridden']]);
-        $response = $this->getHttpClient()->send($request);
+        $response = $this->resources->defaults->headers->get(null, ['headers' => ['x-default-header' => 'Overridden']]);
         $response->getStatusCode()->shouldBe(200);
         $response->getBody()->__toString()->shouldJsonBeEqualTo('Overridden', 'x-default-header');
+    }
+
+    function it_should_return_response_headers()
+    {
+        $response = $this->resources->get();
+        $response->getStatusCode()->shouldBe(200);
+        $response->getBody()->__toString()->shouldEqual('Success');
+        $response->getHeader('X-Powered-By')->shouldEqual(['Express']);
+        $response->getHeader('Content-Type')->shouldEqual(['text/html; charset=utf-8']);
+    }
+
+    function it_should_parse_response_as_text_when_unknown()
+    {
+        $response = $this->resources->responses->text->get();
+        $response->getStatusCode()->shouldBe(200);
+        $response->getBody()->__toString()->shouldEqual('text');
+    }
+
+    function it_should_parse_response_as_json_when_specified()
+    {
+        $response = $this->resources->responses->json->get();
+        $response->getStatusCode()->shouldBe(200);
+        $response->getBody()->__toString()->shouldJsonBeEqualTo(json_decode('{"json": true}', true));
+    }
+
+    function it_should_parse_response_urlencoded_simple_query_string()
+    {
+        $response = $this->resources->responses->urlEncoded->basic->get();
+        $response->getStatusCode()->shouldBe(200);
+        $response->getBody()->__toString()->shouldEqual('key=value');
+    }
+
+    function it_should_parse_response_and_put_duplicate_keys_into_array()
+    {
+        $response = $this->resources->responses->urlEncoded->duplicate->post();
+        $response->getStatusCode()->shouldBe(200);
+        $response->getBody()->__toString()->shouldEqual('key=1&key=2&key=3');
+    }
+
+    function it_should_parse_response_encoded_values_uri_encoded()
+    {
+        $response = $this->resources->responses->urlEncoded->escaped->put();
+        $response->getStatusCode()->shouldBe(200);
+        $response->getBody()->__toString()->shouldEqual('key=Hello%2C%20world!');
     }
 
     public function getMatchers()
