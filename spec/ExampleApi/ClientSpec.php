da4ea5a3fdfd8c373e1fdea235ee11d176828337
Jens Schulze <jens.schulze@commercetools.de>
2015-12-25 15:57:19 +0100

    add more specs

--- spec/ExampleApi/ClientSpec.php
+++ spec/ExampleApi/ClientSpec.php
@@ -12,9 +12,26 @@ use Prophecy\Argument;
 
 class ClientSpec extends ObjectBehavior
 {
-    function validateBaseUriResponse ($response) {
-        $response->getBody()->__toString()->shouldBeEqualTo('Hello World!');
+    protected function camelize($verb)
+    {
+        return lcfirst(
+            implode(
+                '',
+                array_map(
+                    'ucfirst',
+                    array_map(
+                        'strtolower',
+                        explode('-', $verb)
+                    )
+                )
+            )
+        );
+    }
+
+    function validateResponse($response, $body = 'Success')
+    {
         $response->getStatusCode()->shouldBe(200);
+        $response->getBody()->__toString()->shouldEqual($body);
     }
 
     function it_is_initializable()
@@ -22,6 +39,9 @@ class ClientSpec extends ObjectBehavior
         $this->shouldHaveType('ExampleApi\Client');
     }
 
+    /**
+     * base uri tests
+     */
     function it_should_be_able_to_manually_set_the_base_uri()
     {
         $mock = new MockHandler([
@@ -37,7 +57,7 @@ class ClientSpec extends ObjectBehavior
         $this->beConstructedWith($options);
 
         $response = $this->resources->hello->get();
-        $this->validateBaseUriResponse($response);
+        $this->validateResponse($response, 'Hello World!');
     }
 
     function it_should_be_able_to_manually_set_the_base_uri_with_parameters()
@@ -61,50 +81,52 @@ class ClientSpec extends ObjectBehavior
         $this->beConstructedWith($options);
 
         $response = $this->resources->hello->get();
-        $this->validateBaseUriResponse($response);
+        $this->validateResponse($response, 'Hello World!');
     }
 
+    /**
+     * query tests
+     */
     function it_should_append_query_string()
     {
         $response = $this->resources->bounce->url->get(['key' => 'string']);
 
-        $response->getStatusCode()->shouldBe(200);
-        $response->getBody()->__toString()->shouldBeEqualTo('/bounce/url?key=string');
+        $this->validateResponse($response, '/bounce/url?key=string');
     }
 
     function it_should_append_query_string_array()
     {
-        $response = $this->resources->bounce->url->get(null, ['query' => ['key' => [1,2,3]]]);
+        $response = $this->resources->bounce->url->get(null, ['query' => ['key' => [1, 2, 3]]]);
 
-        $response->getStatusCode()->shouldBe(200);
-        $response->getBody()->__toString()->shouldBeEqualTo('/bounce/url?key=1&key=2&key=3');
+        $this->validateResponse($response, '/bounce/url?key=1&key=2&key=3');
     }
 
     function it_should_merge_body_and_query()
     {
         $response = $this->resources->bounce->url->get(
-            ['test' => 'test', 'abc' => 123, 'key' => [1,2,3]],
+            ['test' => 'test', 'abc' => 123, 'key' => [1, 2, 3]],
             ['query' => ['xyz' => '123']]
         );
 
-        $response->getStatusCode()->shouldBe(200);
-        $response->getBody()->__toString()->shouldBeEqualTo('/bounce/url?abc=123&key=1&key=2&key=3&test=test&xyz=123');
+        $this->validateResponse($response, '/bounce/url?abc=123&key=1&key=2&key=3&test=test&xyz=123');
     }
 
     function it_should_be_a_canonical_query()
     {
         $response = $this->resources->bounce->url->get(
             null,
-            ['query' => ['test' => 'test', 'xyz' => '123', 'abc' => 123, 'key' => [1,2,3]]]
+            ['query' => ['test' => 'test', 'xyz' => '123', 'abc' => 123, 'key' => [1, 2, 3]]]
         );
 
-        $response->getStatusCode()->shouldBe(200);
-        $response->getBody()->__toString()->shouldBeEqualTo('/bounce/url?abc=123&key=1&key=2&key=3&test=test&xyz=123');
+        $this->validateResponse($response, '/bounce/url?abc=123&key=1&key=2&key=3&test=test&xyz=123');
     }
 
+    /**
+     * request headers
+     */
     function it_should_pass_custom_headers_with_the_request()
     {
-        $response = $this->resources->bounce->headers->get(null, ['headers' => ['X-Custom-Header' =>  'Custom Header']]);
+        $response = $this->resources->bounce->headers->get(null, ['headers' => ['X-Custom-Header' => 'Custom Header']]);
 
         $response->getStatusCode()->shouldBe(200);
         $response->getBody()->__toString()->shouldJsonBeEqualTo('Custom Header', 'x-custom-header');
@@ -124,6 +146,9 @@ class ClientSpec extends ObjectBehavior
         $response->getBody()->__toString()->shouldJsonBeEqualTo('Overridden', 'x-default-header');
     }
 
+    /**
+     * response headers
+     */
     function it_should_return_response_headers()
     {
         $response = $this->resources->get();
@@ -133,6 +158,9 @@ class ClientSpec extends ObjectBehavior
         $response->getHeader('Content-Type')->shouldEqual(['text/html; charset=utf-8']);
     }
 
+    /**
+     * response body
+     */
     function it_should_parse_response_as_text_when_unknown()
     {
         $response = $this->resources->responses->text->get();
@@ -164,8 +192,132 @@ class ClientSpec extends ObjectBehavior
     function it_should_parse_response_encoded_values_uri_encoded()
     {
         $response = $this->resources->responses->urlEncoded->escaped->put();
-        $response->getStatusCode()->shouldBe(200);
-        $response->getBody()->__toString()->shouldEqual('key=Hello%2C%20world!');
+        $this->validateResponse($response, 'key=Hello%2C%20world!');
+    }
+
+    /**
+     * resource chain
+     */
+    function it_should_support_root_resource()
+    {
+        $response = $this->resources->get();
+        $this->validateResponse($response, 'Success');
+    }
+
+    function it_should_dynamically_generate_resource_chain()
+    {
+        $response = $this->resources->bounce->parameter->variable(123)->get();
+        $this->validateResponse($response, '123');
+    }
+
+    function it_should_output_null_values_as_empty_string()
+    {
+        $response = $this->resources->bounce->parameter->variable(null)->get();
+        $this->validateResponse($response, '');
+    }
+
+    function it_should_use_default_value_when_null()
+    {
+        $response = $this->resources->defaults->parameter->variable(null)->get();
+        $this->validateResponse($response, 'default');
+    }
+
+    function it_should_support_single_parameter_arguments()
+    {
+        $response = $this->resources->parameters->prefix->one(123)->get();
+        $this->validateResponse($response, '123');
+    }
+
+    function it_should_support_not_more_arguments_than_defined()
+    {
+        $response = $this->resources->parameters->prefix->one(123, 456)->get();
+        $this->validateResponse($response, '123');
+    }
+
+    function it_should_dynamically_generate_resource_chain_with_multi_parameteters()
+    {
+        $response = $this->resources->parameters->prefix->three(1, 2, 3)->get();
+        $this->validateResponse($response, '123');
+    }
+
+    function it_should_support_extensions_in_the_resource_chain_for_static_extensions()
+    {
+        $response = $this->resources->extensions->static->json->get();
+        $this->validateResponse($response);
+    }
+
+    function it_should_support_mediaTypeExtension_parameter()
+    {
+        $response = $this->resources->extensions->mediaType->basic->mediaTypeExtension('json')->get();
+        $this->validateResponse($response);
+    }
+
+    function it_should_have_paths_from_enum_values()
+    {
+        $response = $this->resources->extensions->mediaType->enum->json->get();
+        $this->validateResponse($response);
+    }
+
+    function it_should_have_paths_from_period_prefixed_enum_values()
+    {
+        $response = $this->resources->extensions->mediaType->enumPeriod->xml->get();
+        $this->validateResponse($response);
+    }
+
+    function it_should_handle_original_route()
+    {
+        $response = $this->resources->conflicts->mediaType->route->get();
+        $this->validateResponse($response);
+    }
+
+    function it_should_handle_conflict_with_media_type_extension()
+    {
+        $response = $this->resources->conflicts->mediaType->mediaTypeExtension('json')->get();
+        $this->validateResponse($response);
+    }
+
+    /**
+     * Custom resource
+     */
+    function it_support_custom_resource_methods()
+    {
+        $methods = [
+            'get',
+            'post',
+            'put',
+            'head',
+            'delete',
+            'options',
+            'trace',
+            'copy',
+            'lock',
+            'mkcol',
+            'move',
+            'purge',
+            'propfind',
+            'proppatch',
+            'unlock',
+            'report',
+            'mkactivity',
+            'checkout',
+            'merge',
+            'm-search',
+            'notify',
+            'subscribe',
+            'unsubscribe',
+            'patch',
+            'search',
+            'connect'
+        ];
+
+        foreach ($methods as $verb) {
+            if ($verb === 'connect' || $verb === 'head') {
+                continue;
+            }
+            $method = $this->camelize($verb);
+            $response = $this->resource('/status/{id}', ['id' => 200])->$method();
+            $this->validateResponse($response);
+        }
     }
 
     public function getMatchers()
