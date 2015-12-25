10711437cdf5ca790ba2fe2812ab7c95661dc45c
Jens Schulze <jens.schulze@commercetools.de>
2015-12-25 16:05:09 +0100

    update gulp file for php spec tests

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var server = require('./test/support/server')
 
 var MOCHA_BIN_PATH = join(__dirname, 'node_modules/mocha/bin/_mocha')
 var CLIENT_OUT_DIR = join(__dirname, 'test/.tmp')
-var RAML_CLIENT_SCRIPT = join(__dirname, 'bin/raml-javascript-generator.js')
+var RAML_CLIENT_SCRIPT = join(__dirname, 'bin/raml-php-generator.js')
 
 gulp.task('server', function (done) {
   var app = server.listen(4444, done)
@@ -46,54 +46,22 @@ gulp.task('generate', function (done) {
   })
 })
 
-gulp.task('test:node', [
-  'server',
-  'generate'
-], function () {
-  var testProcess = spawn(MOCHA_BIN_PATH, [
-    join(__dirname, 'test/node/**/*.js'),
-    '-R',
-    'spec',
-    '--require',
-    join(__dirname, 'test/support/globals.js')
-  ])
-
-  testProcess.stdout.pipe(process.stdout)
-  testProcess.stderr.pipe(process.stderr)
-
-  return testProcess.stdout
-})
-
 gulp.task('test:php', [
   'server',
   'generate'
 ], function (done) {
 
   exec('vendor/bin/phpspec run ', function (err, stdout, stderr) {
+    process.stdout.write(stdout)
+    process.stderr.write(stderr)
     if (err) {
       return done(err)
     }
-
-    process.stdout.write(stdout)
-    process.stderr.write(stderr)
 
     done()
   })
 })
 
-gulp.task('test:browser', [
-  'server',
-  'generate'
-], function (done) {
-  var opts = {
-    singleRun: true,
-    configFile: join(__dirname, 'karma.conf.js')
-  }
-
-  return karma.start(opts, done)
-})
-
 gulp.task('test', [
-  'test:node',
-  'test:browser'
+  'test:php'
 ])
