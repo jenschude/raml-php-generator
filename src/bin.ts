e11752cb7c2cd5eb5d19e5e77b7839ea6006f125
Jens Schulze <jens.schulze@commercetools.de>
2016-08-05 10:40:41 +0200

    cleanup dependencies

--- src/bin.ts
+++ src/bin.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-
+declare var require: any;
+declare var process: any;
 import { bin } from 'raml-generator/bin'
 import { client } from './index'
+var pkg = require('../package.json');
 
-bin(client, require('../package.json'), process.argv)
+bin(client, pkg, process.argv)
--- src/support/feature.ts
+++ src/support/feature.ts
@@ -1,4 +1,4 @@
-
+declare var process: any;
 
 export function supportStrictTypes () {
     return process.env.USE_STRICT_TYPES == 'true';
